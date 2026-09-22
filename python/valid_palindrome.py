7715f6bb454e05bd251117db6ddcf2a38b14d004
Olabosoye olamide <olabosoyeolamide123@gmail.com>
2026-09-22 22:05:55 +0100

    LeetCode: update python/valid_palindrome.py

---
+++ python/valid_palindrome.py
@@ -0,0 +1,26 @@
+# ======================================
+# LeetCode Problem: valid palindrome
+# Language: python3
+# Link: https://leetcode.com/problems/valid-palindrome/
+# Synced by: LinkCode
+# Date: 9/22/2026, 10:05:54 PM
+# ======================================
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
